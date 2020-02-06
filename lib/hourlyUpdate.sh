1fcd0ed12fcf097f278c9f43042fb8c1a2b8e409
StephenCole1 <StephenCole@ibm.com>
2020-02-06 11:30:34 -0400

    Refactored HourlyUpdate
    rewrote some of the ugly bash

--- lib/hourlyUpdate.sh
+++ lib/hourlyUpdate.sh
@@ -1,18 +1,38 @@
 # HourlyUpdate V1.2.1
 # Usage: bash hourlyUpdate.sh &
 
-# Get default Browser
-browser=`x=~/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist;\ 
-plutil -convert xml1 $x;\
-grep 'https' -b3 $x | awk 'NR==2 {split($2, arr, "[><]"); print arr[3]}';` #plutil -convert binary1 $x`
+#------------------------------------------------------------------------------------
 
-browserEX=""
+#Get default browser
+get_default () {
+	browser=`x=~/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist;\ 
+		plutil -convert xml1 $x;\
+		grep 'https' -b3 $x | awk 'NR==2 {split($2, arr, "[><]"); print arr[3]}';`	
+}
 
-if [[ $browser == "com.google.chrome" ]] ; then
-	browserEX="google"
-else
-	browserEX="firefox"
-fi
+#Parses browser package to return a code to send to open-window.py
+check_browser () {
+	if [[ $1 == "com.google.chrome" ]] ; then
+		browserEx="google"
+	else
+		browserEx="firefox"
+	fi	
+}
+
+#Terminal beeps
+beep () {
+	for x in {1..10}; do
+		echo "beep"
+		echo -en "\007"
+		sleep 1
+	done
+}
+
+#-------------------------------------main------------------------------------------
+
+get_default
+
+check_browser $browser
 
 echo "Start work timer!"
 
@@ -23,23 +43,17 @@ hour=`date +'%I'`
 while [[ $currentTime != "5:00" ]]; do
 	currentTime=`date +'%I:%M'`
 	
-	alertTimes=("09:30" "10:00" "11:00" "12:00" "01:00" "02:00" "03:00" "04:00" "04:05" "04:10" "05:00")
+	alertTimes=("09:30" "10:00" "11:00" "12:00" "01:00" "02:00" "03:00" "04:00" "04:10" "05:00")
 	
 	for t in ${alertTimes[@]}; do
-		#echo "Current: $currentTime , Alert:$t"
 		if [[ $currentTime ==	$t ]]; then
 			
-			python3 ./lib/open-window.py $browserEX
+			python3 ./lib/open-window.py $browserEx
 			python3 ./lib/move-mouse.py			
-	
-			for x in {1..10}; do
-				echo "beep"
-				echo -en "\007"
-				sleep 1
-			done
+
+			beep
 
 			echo "See you in an hour!!"
-
 		fi
 	done
 
@@ -48,3 +62,6 @@ while [[ $currentTime != "5:00" ]]; do
 done
 
 echo "Time to Leave!"
+
+
+#-----------------------------------------------------------------------------
